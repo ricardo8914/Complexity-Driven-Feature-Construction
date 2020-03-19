ea9fc1baad57d09040ca3963ab926e1fddfacfba
ricardo salazar <ricardosalazar@ip-192-168-0-103.eu-central-1.compute.internal>
2020-03-19 19:34:27 +0100

    added a new objective, number of features

--- new_project/test_evolutionary.py
+++ new_project/test_evolutionary.py
@@ -63,12 +63,13 @@ def evolution(X_train, y_train, scorers=[], cv_splitter=5, max_search_time=60):
 		for scorer_i in range(len(scorers)):
 			results.append(cv.cv_results_['mean_test_' + 'scorer' + str(scorer_i)][0])
 
+		results.append(np.sum(features))
 		return results
 
 	class MyProblem(Problem):
 
 		def __init__(self):
-			number_objectives = len(scorers)
+			number_objectives = 3
 
 			super().__init__(n_var=X_train.shape[1],
 							 n_obj=number_objectives,
@@ -91,19 +92,19 @@ def evolution(X_train, y_train, scorers=[], cv_splitter=5, max_search_time=60):
 
 
 
-	population_size = 100
+	population_size = 20
 	cross_over_rate = 0.9
 	algorithm = NSGA2(pop_size=population_size,
 					  sampling=get_sampling("bin_random"),
 					  crossover=get_crossover('bin_one_point'),
 					  # get_crossover("bin_hux"),#get_crossover("bin_two_point"),
 					  mutation=BinaryBitflipMutation(1.0 / X_train.shape[1]),
-					  elimate_duplicates=True,
+					  elimate_duplicates=True#,
 					  # n_offsprings= cross_over_rate * population_size
 					  )
 
-	#res = minimize(problem, algorithm, ('n_gen', 10), disp=False)
-	res = minimize(problem, algorithm, get_termination("n_eval", 5), disp=False)
+	res = minimize(problem, algorithm, get_termination('n_gen', 50), disp=False)
+	#res = minimize(problem, algorithm, get_termination("n_eval", 5), disp=False)
 
 	return res.X
 
@@ -113,17 +114,23 @@ def evolution(X_train, y_train, scorers=[], cv_splitter=5, max_search_time=60):
 	#plt.show()
 
 
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-
-X, y = datasets.make_classification(n_samples=1000, n_features=10, n_informative=2, n_redundant=1)
-
-auc_scorer = make_scorer(roc_auc_score, greater_is_better=True, needs_threshold=True)
-f1_scorer = make_scorer(f1_score)
-# # #fair_train = make_scorer(true_positive_rate_score, greater_is_better=True, sensitive_data=X_train[:, sensitive_ids[0]])
-# #
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-a = evolution(X_train, y_train, scorers=[auc_scorer, f1_scorer], cv_splitter=5, max_search_time=1000)
-
-print(a)
+# from sklearn import datasets
+# from sklearn.model_selection import train_test_split
+#
+# X, y = datasets.make_classification(n_samples=1000, n_features=10, n_informative=2, n_redundant=1)
+#
+# auc_scorer = make_scorer(roc_auc_score, greater_is_better=True, needs_threshold=True)
+# f1_scorer = make_scorer(f1_score)
+# # # #fair_train = make_scorer(true_positive_rate_score, greater_is_better=True, sensitive_data=X_train[:, sensitive_ids[0]])
+# # #
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#
+# a = evolution(X_train, y_train, scorers=[auc_scorer, f1_scorer], cv_splitter=5, max_search_time=1000)
+#
+#
+# # for i in a:
+# # # 	x = np.argwhere(i)
+# # # 	s = []
+# # # 	for idj, j in enumerate(x):
+# # # 		s.extend([x.item(idj)])
+# # # 	print(s)
